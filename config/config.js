37da1d5ce61c324cd315d24cb4a9521d6779f8d3
Osama Alibrahim <os778ma@gmail.com>
2024-05-07 20:51:34 +0300

    deploy

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-{
+module.exports ={
   "development": {
     "username": "root",
     "password": null,
-    "database": "Telescope",
+    "database": "Vista",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
@@ -14,10 +14,10 @@
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
   }
 }
